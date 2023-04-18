2c9fdcad975c21fa1d76abf4a5e873c6dfc935c8
alejanicki <alessandrojanicki@hotmail.com>
2023-04-18 19:11:07 -0300

    Correção de messagens de erro e criação do view password

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -8,21 +8,41 @@ const LoginPage = () => {
   const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
   const [passwordType, setPasswordType] = useState("password");
-  const [passwordError, setpasswordError] = useState("");
-  const [inputsError, setInputsError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
 
   //Handling event on subtmit button
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    //Checking if is a valid email and a strong password
-    if (validator.isEmail(email) && validator.isStrongPassword(password)) {
-      console.log({ email, password });
-      setEmailError("Email Valido");
-      setpasswordError("Senha Forte");
+    //Checkin if there are blank inputs 
+    if (validator.isEmpty(email) && validator.isEmpty(password)) {
+        setEmailError("Por favor, insira um email válido");
+        setPasswordError("Por favor, insira uma senha mais forte");
+        return
+    }
+
+    //Checking if is not a valid email
+    if (!validator.isEmail(email)) {
+        setEmailError("Por favor, insira um email válido");
+        return
+    }
+    setEmailError("");
+
+    //Checking if is not a strong password
+    if (!validator.isStrongPassword(password)){
+        setPasswordError("Por favor, insira uma senha mais forte");
+        return
+    }
+    setPasswordError("");
+    console.log({email, password})
+  };  
+
+  //Function to toggle password view
+  const togglePassword = () => {
+    if (passwordType === "password") {
+        setPasswordType("text");
     } else {
-      setEmailError("Email Invalido");
-      setpasswordError("Senha Fraca");
+        setPasswordType("password");
     }
   };
 
@@ -57,10 +77,9 @@ const LoginPage = () => {
               setPassword(e.target.value);
             }}
           />
-          <input type="checkbox" name="togglePassword" id="togglePassword" />
+          <input type="checkbox" name="togglePassword" id="togglePassword" onChange={togglePassword}/>
           <small>{passwordError}</small>
         </div>
-        <small>{inputsError}</small>
         <input type="submit" value="Entrar" />
       </form>
     </div>
