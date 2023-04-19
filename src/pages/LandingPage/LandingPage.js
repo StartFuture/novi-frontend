6ff5b7e6067447ce22ec9e6fb4f2335170f440f7
Thiago Yuske <thiagoyuske@outlook.com>
2023-04-19 20:50:48 -0300

    feat(LandingPage) - Adding images

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,11 +2,14 @@ import React from "react";
 
 import Footer from '../../components/Footer.js';
 import LandingPageInicio from '../../components/LandingPageInicio.js'
+import LandingUs from '../../components/landingUs.js'
 
 function LandingPage() {
     return (
         <div className='Landing-page'>
           <LandingPageInicio/>
+          <br/>
+          <LandingUs/>
           <br/>
           <Footer />
         </div>
