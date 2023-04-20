5508b86ff8de2b8a9b76761301f85a82d59370ce
Thiago Yuske <thiagoyuske@outlook.com>
2023-04-20 20:25:42 -0300

    fix(LandingPage) - Adjusting css of elements in general

--- src/components/landingPageTop.js
+++ src/components/landingPageTop.js
@@ -2,13 +2,15 @@ import React from "react";
 
 import '../assets/css/landingPageTop.css'
 
+import { Link } from 'react-router-dom';
+
 function LandingPageTop(){
     return (
         <div className="LPHeader">
             <header>
                 <div className="LPNavbar">
                     <h4 id="noviLogoNavbar"> Noví </h4>
-                    <button className="btnLandingPage"> Entrar/Registrar </button>
+                    <button className="btnLandingPage"><Link to='/login' className="link">Entrar/Registrar</Link></button>
                 </div>
             </header>
             <div className="LPBanner">
