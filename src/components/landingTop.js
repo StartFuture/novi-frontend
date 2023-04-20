71a9e9186d67e80f612323915bb1a3dd9f96be5d
Thiago Yuske <thiagoyuske@outlook.com>
2023-04-19 21:32:51 -0300

    refactor(css) - adding tags and className to identify items on HTML

--- src/components/landingTop.js
+++ src/components/landingTop.js
@@ -7,13 +7,13 @@ function LandingPageInicio(){
         <div className="LPHeader">
             <header>
                 <div className="LPNavbar">
-                    <h4 className="noviLogo"> Noví </h4>
-                    <button className="entrarRegistrar"> Entrar/Registrar </button>
+                    <h4 id="noviLogoNavbar"> Noví </h4>
+                    <button className="btnLandingPage"> Entrar/Registrar </button>
                 </div>
             </header>
             <div className="LPBanner">
                 <p id="bannerPhrase"><b>Descubra</b> destinos emocionantes para explorar</p>
-                <button> Quero viajar > </button>
+                <button className="btnLandingPage"> Quero viajar > </button>
             </div>
         </div>
     )
