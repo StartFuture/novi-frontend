46abbd6e6eb5ed2f60586ddb09beacf6c1fddb2b
alejanicki <alessandrojanicki@hotmail.com>
2023-04-23 14:33:37 -0300

    Conclusão CSS e implementação de função de estrelas

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -5,6 +5,8 @@ import Footer from "../../components/footer.js";
 import LandingTop from "../../components/landingPageTop.js";
 import "../../assets/css/LandingPage.css";
 
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
+
 function LandingPage() {
   const [comments, setComments] = useState([]);
 
@@ -21,22 +23,148 @@ function LandingPage() {
   function ShowComment(props) {
     const comments = props.comments;
 
+    const countStars = (cardStars) => {
+      if (cardStars === 5) {
+        return (
+          <>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+          </>
+        );
+      } else if (cardStars === 4) {
+        return (
+          <>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+          </>
+        );
+      } else if (cardStars === 3) {
+        return (
+          <>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+          </>
+        );
+      } else if (cardStars === 2) {
+        return (
+          <>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+          </>
+        );
+      } else if (cardStars === 1) {
+        return (
+          <>
+            <h4>
+              <AiFillStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+          </>
+        );
+      } else {
+        return (
+          <>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+            <h4>
+              <AiOutlineStar />
+            </h4>
+          </>
+        );
+      }
+    };
+
     const listComments = [];
     for (let i = 0; i < comments.length; i++) {
       listComments.push(
         <li key={i} className="lpCommentsCards">
           <div id="commentsCardsGrid1">
             <div id="commentsGrid1ImgName">
-                <p id="cardImg">{JSON.stringify(comments[i].img)}</p>
+              <p id="cardImg">{comments[i].img}</p>
               <div>
-                <p id="cardName">{JSON.stringify(comments[i].name)}</p>
-                <p id="cardPerfil">{JSON.stringify(comments[i].perfil)}</p>
+                <p id="cardName">{comments[i].user_name}</p>
+                <p id="cardPerfil">
+                  <strong>{comments[i].perfil}</strong>
+                </p>
               </div>
             </div>
-            <p id="cardStars">{JSON.stringify(comments[i].stars)}</p>
+            <p id="cardStars">{countStars(comments[i].stars)}</p>
           </div>
           <div id="commentsCardsGrid2">
-            <p id="cardComment">{JSON.stringify(comments[i].comment)}</p>
+            <p id="cardComment">{comments[i].comment}</p>
           </div>
         </li>
       );
