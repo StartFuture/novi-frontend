fe5ded7227627671478028c6acb860fd7292d436
henrique <henriquebayer8@gmail.com>
2023-05-10 17:36:41 -0300

    task consulta e validaçao de CEP feita

--- src/components/inputs.jsx
+++ src/components/inputs.jsx
@@ -1,7 +1,31 @@
+import { useCallback } from 'react'
 import styles from '../assets/css/inputs.module.css'
 import LogoNovi from '../pages/registerPage/assets/image/Noví(1).png'
 
-export default function Inputs() {
+
+export default function Inputs(props) {
+
+//Mascara do CEP
+//99999-999
+//REGEX
+const handleKeyUp = useCallback((e) => {
+    e.currentTarget.maxLength = 9;
+    let value = e.currentTarget.value;
+    value = value.replace(/\D/g, "")
+    value = value.replace(/^(\d{5})(\d)/, "$1-$2")
+    e.currentTarget.value = value;
+}, []);
+
+//check CEP
+
+const checkCEP = (e) => 
+{
+const cep = e.target.value.replace(/\D/g, '')
+console.log(cep);
+fetch(`https://viacep.com.br/ws/${cep}/json/`).then(res => res.json()).then(data => {console.log(data);
+
+});
+}
 
     return (
 
@@ -39,7 +63,7 @@ export default function Inputs() {
 
                             <div className={styles["first-section"]}>
                                 <label>CEP</label>
-                                <input placeholder='Digite aqui'></input>
+                                <input placeholder='99999-999' name='cep' onKeyUp={handleKeyUp} onBlur={checkCEP}></input>
 
                                 <div className={styles["second-section"]}>
 
