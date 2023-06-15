0715e6610a1e8018202c2b0339ebf869a3fa8145
henrique <henriquebayer8@gmail.com>
2023-06-15 18:23:47 -0300

    correcao do calendario na pagina result-trip

--- src/pages/resultTrip/index.jsx
+++ src/pages/resultTrip/index.jsx
@@ -193,113 +193,132 @@ function ResultTrip() {
               </div>
             </div>
             <div className="tripCalendar">
-              <Calendar>
-                <div className="month"><div>Janeiro <span className="year">2023</span> <a><IoIosArrowBack className="arrowLPosition"></IoIosArrowBack></a><a><IoIosArrowForward className="arrowRPosition"></IoIosArrowForward></a></div></div>
-                <div className="days">
-                  <span>Seg</span>
-                  <span>Ter</span>
-                  <span>Quar</span>
-                  <span>Quin</span>
-                  <span>Sex</span>
-                  <span>Sab</span>
-                  <span>Dom</span>
+            <Calendar>
+              <div className="month">
+                <div>January
+                  <span className="year">2019
+                  </span>
                 </div>
-                <div className="dates">
-                  <button>
-                    <time>1</time>
-                  </button>
-                  <button>
-                    <time>2</time>
-                  </button>
-                  <button>
-                    <time>3</time>
-                  </button>
-                  <button>
-                    <time>4</time>
-                  </button>
-                  <button>
-                    <time>5</time>
-                  </button>
-                  <button>
-                    <time>6</time>
-                  </button>
-                  <button>
-                    <time>7</time>
-                  </button>
-                  <button>
-                    <time>8</time>
-                  </button>
-                  <button>
-                    <time>9</time>
-                  </button>
-                  <button>
-                    <time>10</time>
-                  </button>
-                  <button>
-                    <time>11</time>
-                  </button>
-                  <button>
-                    <time>12</time>
-                  </button>
-                  <button>
-                    <time>13</time>
-                  </button>
-                  <button>
-                    <time>14</time>
-                  </button>
-                  <button>
-                    <time>15</time>
-                  </button>
-                  <button>
-                    <time>16</time>
-                  </button>
-                  <button>
-                    <time>17</time>
-                  </button>
-                  <button>
-                    <time>18</time>
-                  </button>
-                  <button>
-                    <time>19</time>
-                  </button>
-                  <button>
-                    <time>20</time>
-                  </button>
-                  <button>
-                    <time>21</time>
-                  </button>
-                  <button>
-                    <time>22</time>
-                  </button>
-                  <button>
-                    <time>23</time>
-                  </button>
-                  <button>
-                    <time>24</time>
-                  </button>
-                  <button>
-                    <time>25</time>
-                  </button>
-                  <button>
-                    <time>26</time>
-                  </button>
-                  <button>
-                    <time>27</time>
-                  </button>
-                  <button>
-                    <time>28</time>
-                  </button>
-                  <button>
-                    <time>29</time>
-                  </button>
-                  <button>
-                    <time>30</time>
-                  </button>
-                  <button>
-                    <time>31</time>
-                  </button>
+                <div className="flexArrow">
+                  <a><IoIosArrowBack className="arrowLPosition"></IoIosArrowBack></a>
+                  <a><IoIosArrowForward className="arrowRPosition"></IoIosArrowForward></a>
                 </div>
-              </Calendar>
+              </div>
+              <div className="days">
+                <span>Seg</span>
+                <span>Ter</span>
+                <span>Quar</span>
+                <span>Quin</span>
+                <span>Sex</span>
+                <span>Sab</span>
+                <span>Dom</span>
+              </div>
+              <div className="dates">
+                <button>
+                  <time>1</time>
+                </button>
+                <button>
+                  <time>2</time>
+                </button>
+                <button>
+                  <time>3</time>
+                </button>
+                <button>
+                  <time>4</time>
+                </button>
+                <button>
+                  <time>5</time>
+                </button>
+                <button>
+                  <time>6</time>
+                </button>
+                <button>
+                  <time>7</time>
+                </button>
+                <button>
+                  <time>8</time>
+                </button>
+                <button>
+                  <time>9</time>
+                </button>
+                <button>
+                  <time>10</time>
+                </button>
+                <button>
+                  <time>11</time>
+                </button>
+                <button>
+                  <time>12</time>
+                </button>
+                <button>
+                  <time>13</time>
+                </button>
+                <button>
+                  <time>14</time>
+                </button>
+                <button>
+                  <time>15</time>
+                </button>
+                <button>
+                  <time>16</time>
+                </button>
+                <button>
+                  <time>17</time>
+                </button>
+                <button>
+                  <time>18</time>
+                </button>
+                <button>
+                  <time>19</time>
+                </button>
+                <button>
+                  <time>20</time>
+                </button>
+                <button>
+                  <time>21</time>
+                </button>
+                <button>
+                  <time>22</time>
+                </button>
+                <button>
+                  <time>23</time>
+                </button>
+                <button>
+                  <time>24</time>
+                </button>
+                <button>
+                  <time>25</time>
+                </button>
+                <button>
+                  <time>26</time>
+                </button>
+                <button>
+                  <time>27</time>
+                </button>
+                <button>
+                  <time>28</time>
+                </button>
+                <button>
+                  <time>29</time>
+                </button>
+                <button>
+                  <time>30</time>
+                </button>
+                <button>
+                  <time>31</time>
+                </button>
+              </div>
+
+
+              <div className="travelInfoBox">
+                <div className="pointStyle"></div>
+                <div className="alignItems">
+                  <h2>Rio de Janeiro</h2>
+                  <p>14 dias</p>
+                </div>
+              </div>
+            </Calendar>
+
             </div>
           </div>
           <Footer>
