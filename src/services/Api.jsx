d7a25b0307ad7ac8ef9573df4c3c9193fa206179
alejanicki <alessandrojanicki@hotmail.com>
2023-06-15 01:48:39 -0300

    feat: jwt flux

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,13 +1,16 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://api.novi.startfuture.com.br",
+  baseURL: "https://api.novi.startfuture.com.br",
 });
 
 export const authLogin = async (email, password_user) => {
-    return api.post("/auth/login", {email, password_user});
+  const params = new URLSearchParams();
+  params.append("username", email);
+  params.append("password", password_user);
+  return api.post("/auth/login", params);
 };
 
 export const cepCheck = (cep) => {
-    return axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-}
+  return axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+};
