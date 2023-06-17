bd9eacb216d0a8e3d74ca617c4f421f1e9891baf
alejanicki <alessandrojanicki@hotmail.com>
2023-06-17 00:51:38 -0300

    fix: landing page

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { api } from "../../services/Api";
-import { DivStyleTop } from "pages/landingPage/components/CarrouselTop/style"
+import { DivStyleTop } from "pages/landingPage/components/CarrouselTop/style";
 
 import Footer from "../../components/footer";
 import Carrosel from "../../components/carrosel";
@@ -9,10 +9,7 @@ import LandingTop from "./components/landingPageTop";
 import "./assets/css/landingPage.css";
 import CarrosselComment from "./components/CarrosselComment/CarroselComment";
 import CarrouselTop from "./components/CarrouselTop/carrouselTop";
-import DescubraNovasExperiencias from "./components/CarrouselTop/CarrouselTopText/DescubraNovasExperiencias"
-
-
-
+import DescubraNovasExperiencias from "./components/CarrouselTop/CarrouselTopText/DescubraNovasExperiencias";
 
 function LandingPage() {
   const [comments, setComments] = useState([]);
@@ -22,7 +19,8 @@ function LandingPage() {
   console.log(localStorage.getItem("hasTrip"));
 
   useEffect(() => {
-    api.get("/get-comment")
+    api
+      .get("/get-comment")
       .then((response) => setComments(response.data))
       .catch((err) => {
         console.log("error: " + err);
@@ -32,27 +30,25 @@ function LandingPage() {
   console.log(comments);
 
   return (
-    <div className="lp">
-      <div>
-        <LandingTop />
-      </div>
+    <div className="main">
+      <div className="lp">
+        <div>
+          <LandingTop />
+        </div>
 
-      <div className="carroselTopContainer">
-       
+        <div className="carroselTopContainer">
           <DescubraNovasExperiencias></DescubraNovasExperiencias>
-        
-        
           <CarrouselTop></CarrouselTop>
-        
-      </div>
-      <div>
-        <LandingPageUs />
-      </div>
-      <div>
-        <CarrosselComment></CarrosselComment>
-      </div>
-      <div>
-        <Footer />
+        </div>
+        <div>
+          <LandingPageUs />
+        </div>
+        <div>
+          <CarrosselComment></CarrosselComment>
+        </div>
+        <div>
+          <Footer />
+        </div>
       </div>
     </div>
   );
